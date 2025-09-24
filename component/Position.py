9cb97b568ac598483b46f3efc07733fd0abb2763
Charlotte Germe <charlotte.germe16@gmail.com>
2025-09-24 16:35:29 +0200

    add dragon movement + update attributs

--- component/Position.py
+++ component/Position.py
@@ -1,9 +1,9 @@
 class Position:
 
-    def __init__(self, x : int, y : int):
+    def __init__(self, x: int, y: int):
         """x et y sont les coordonnées sr la grille"""
-        self.x : int = x
-        self.y : int = y
+        self.x: int = x
+        self.y: int = y
 
     def move(self, x: int, y: int) -> None:
         """
@@ -15,13 +15,13 @@ class Position:
             None: Met à jour l'objet Position directement
         """
         self.x += x
-        self.y += self.y + y
+        self.y += y
 
-    def get_x(self):
+    def get_x(self) -> int:
         """Retourne le x de la position"""
         return self.x
 
-    def get_y(self):
+    def get_y(self) -> int:
         """Retourne le y de la position"""
         return self.y
 
@@ -31,7 +31,6 @@ class Position:
 
 
 if __name__ == '__main__':
-
     # Création de positions
     pos1 = Position(0, 0)
     pos2 = Position(5, 10)
@@ -44,8 +43,7 @@ if __name__ == '__main__':
 
     # Modification de la position
     print("Test mèthode move")
-    pos2.move(3,2)
+    pos2.move(3, 2)
     print(f"Postion 2 (+3x -2y): {pos2}")
-    pos3.move(-12,2)
+    pos3.move(-12, 2)
     print(f"Postion 3 (-12x, +2y): {pos3}")
-
