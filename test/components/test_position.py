518e36f8816bb2afb4984b7ea66e8b6a4ef753bf
Yanis <yanis.arabah@gmail.com>
2025-09-24 16:57:44 +0200

    fix(test position.py): fix import error

--- test/components/test_position.py
+++ test/components/test_position.py
@@ -1,20 +1,24 @@
 import pytest
 
-from ... import position
+from ...component import position
 
 class TestPosition:
     
-    def steup_method(self):
+    def setup_method(self):
         """Initialisation précédant chaque test effectué"""
         self.position = position.Position(0, 0)
 
     def test_initial_position(self):
         """Test de la position initiale"""
-        assert self.position.get_coordinates() == (0, 0)
+        assert self.position.get_x() == 0
+        assert self.position.get_y() == 0
 
     def test_move(self):
         """Test du déplacement"""
         self.position.move(2, -3)
-        assert self.position.get_coordinates() == (2, -3)
-        self.posistion.move(-2, 3)
-        assert self.position.get_coordinates() == (0, 0)
+        assert self.position.get_x() == 2
+        assert self.position.get_y() == -3
+
+        self.position.move(-2, 3)
+        assert self.position.get_x() == 0
+        assert self.position.get_y() == 0
