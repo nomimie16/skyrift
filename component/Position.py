4548ac162b2dad7ec257b23f3eb5f919d6feab38
Charlotte Germe <charlotte.germe16@gmail.com>
2025-09-24 09:09:44 +0200

    update (position.py): add move method

--- component/Position.py
+++ component/Position.py
@@ -1,7 +1,10 @@
-
 class Position:
 
     def __init__(self, x, y):
         """x et y sont les coordonnées sr la grille"""
         self.x = x
         self.y = y
+
+    def move(self, x, y):
+        """Retourne nouvelle position après déplacement"""
+        return Position(self.x + x, self.y + y)
