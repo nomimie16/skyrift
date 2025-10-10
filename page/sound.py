2e1929fbbf735783ddb2a465e40bf84d9574f528
Yanis <yanis.arabah@gmail.com>
2025-10-10 11:16:54 +0200

    update sound

--- page/sound.py
+++ page/sound.py
@@ -1,5 +1,7 @@
 import pygame
 
+SOUND_ASSETS_PATH = "assets/sounds/"
+
 class SoundManager():
     """classe facilitant la gestion du son"""
 
@@ -8,5 +10,5 @@ class SoundManager():
 
     def play_sound(self, sound_file_name: str):
         """Joue le son passé en paramètre, seul le nom du fichier est nécessaire"""
-        sound = pygame.mixer.Sound("assets/sounds/" + sound_file_name)
+        sound = pygame.mixer.Sound(SOUND_ASSETS_PATH + sound_file_name)
         sound.play()
