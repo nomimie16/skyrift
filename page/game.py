2f842a013fc6258da616e20a3026fc5fbec93a2d
liiloouu16 <li.chatelain@hotmail.fr>
2025-11-24 13:49:03 +0100

    maj bug

--- page/game.py
+++ page/game.py
@@ -1,8 +1,5 @@
-##########################
-#FENETRE PRINCIPALE DU JEU
-##########################
-
 import pygame
+from sidepanels import draw_sidepanels
 
 def run_game(screen, ui):
     WHITE = (240, 240, 240)
@@ -12,23 +9,38 @@ def run_game(screen, ui):
     img_test_rect = img_test.get_rect()
     img_test_rect.topleft = (100,100)
 
-    while running:
-        for event in pygame.event.get():
-            #si on ferme la fenêtre
-            if event.type == pygame.QUIT:
-                return "quit"
-            #
-            action = ui.handle_event(event)
-            if action == "quit":
-                return "quit"
-            if action == "settings":
-                print("Ouvrir paramètres depuis le jeu")
-                return "settings"
+    # État des panneaux
+    left_open = False
+    right_open = False
 
-        #Dessiner le jeu 
+    while running:
+        
+        # Dessiner le jeu
         screen.fill(WHITE)
         ui.draw(screen)
-        screen.blit(img_test,img_test_rect)
+        screen.blit(img_test, img_test_rect)
+
+        # Dessiner les side panels et récupérer leurs rectangles
+        left_rect, right_rect = draw_sidepanels(screen, left_open, right_open)
+        
+        for event in pygame.event.get():
+            action = ui.handle_event(event)
+            if action == "pause":
+                return "pause" 
+        
+        # Gérer l'ouverture/fermeture des panneaux
+        mouse = pygame.mouse.get_pos()
+        if left_rect.collidepoint(mouse):
+            left_open = True
+        else:
+            left_open = False
+
+        if right_rect.collidepoint(mouse):
+            right_open = True
+        else:
+            right_open = False
+ 
+
         pygame.display.flip()
 
     return None
--- page/ui.py
+++ page/ui.py
@@ -1,36 +1,42 @@
-##########################
-#MODULE D'INTERFACE COMMUN
-##########################
+############################
+#MODULE D'INTERFACE COMMUN #
+############################
 
 import pygame
 
 class UIOverlay:
     def __init__(self, screen):
-        '''Chargement des icons'''
-        # Bouton quitter le jeu
-        self.quit_img = pygame.image.load("assets/img/quit.png").convert_alpha()
-        self.quit_img = pygame.transform.scale(self.quit_img, (55, 55))
-        self.quit_rect = self.quit_img.get_rect()
-        self.quit_rect.topleft = (screen.get_width() - 50, 0)
-        # Bouton settings 
-        self.settings_img = pygame.image.load("assets/img/settings.png").convert_alpha()
-        self.settings_img = pygame.transform.scale(self.settings_img, (55, 55))
-        self.settings_rect = self.settings_img.get_rect()
-        self.settings_rect.topleft = (screen.get_width() - 90, 0)
+        '''Chargement des icons'''        
+        # Bouton pause
+        self.pause_btn = pygame.image.load("assets/img/pause_button.png").convert_alpha()
+        self.pause_btn = pygame.transform.scale(self.pause_btn, (55, 55))
+        self.pause_rect = self.pause_btn.get_rect()
+        self.pause_rect.topright = (screen.get_width() - 10, 0)
         
+        # Monnaie
+        self.coin_img = pygame.image.load("assets/img/coin.png").convert_alpha()
+        self.coin_img = pygame.transform.scale(self.coin_img, (50, 50))
+        self.coin_rect = self.coin_img.get_rect()
+        self.coin_rect.topleft = (10, 10)
+        self.coin_value = 0
+        self.coin_text = pygame.font.Font(None, 50).render(str(self.coin_value), True, (255, 215, 0))
+        self.coin_text_rect = self.coin_text.get_rect()
+        self.coin_text_rect.topleft = (70, 20)
+        
+        # Joueur en cours
+        self.current_player = pygame.font.Font(None, 36).render("Joueur 1", True, (0, 0, 0))
+        self.current_player_rect = self.current_player.get_rect(center=(screen.get_width() // 2, 35))
+
     def draw(self, screen):
         '''Fonction dessine les images/icons'''
-        screen.blit(self.quit_img, self.quit_rect)
-        screen.blit(self.settings_img, self.settings_rect)
-        
+        screen.blit(self.pause_btn, self.pause_rect)
+        screen.blit(self.coin_img, self.coin_rect)
+        screen.blit(self.coin_text, self.coin_text_rect)
+        screen.blit(self.current_player, self.current_player_rect)
+
     def handle_event(self, event):
         '''Retourne une action selon le bouton cliqué'''
         if event.type == pygame.MOUSEBUTTONDOWN:
-            #Bouton quitter cliqué
-            if self.quit_rect.collidepoint(event.pos):
-                print("Jeu SkyRift Fermé.\n")
-                return 'quit'
-            #Bouton setting cliqué
-            if self.settings_rect.collidepoint(event.pos):
-                print("Ouverture des paramètres.\n")
-                return 'settings'
+            #Bouton pause cliqué
+            if self.pause_rect.collidepoint(event.pos):
+                return 'pause'
